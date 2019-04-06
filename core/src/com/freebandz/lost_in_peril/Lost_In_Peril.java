2941c7a55f4cc35da5ae0d4b81b5fcf286da669d
Timothy Mardesen <tg-mardesen@wiu.edu>
2019-04-06 15:54:47 -0500

    FLUSHING TO NEW MAIN

--- core/src/com/freebandz/lost_in_peril/Lost_In_Peril.java
+++ core/src/com/freebandz/lost_in_peril/Lost_In_Peril.java
@@ -2,6 +2,7 @@ package com.freebandz.lost_in_peril;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,6 +10,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Lost_In_Peril extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	float x,y;
 	
 	@Override
 	public void create () {
@@ -20,8 +22,23 @@ public class Lost_In_Peril extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		//place code here:
+		
+		if(Gdx.input.isKeyPressed(Keys.UP)) {
+			y+=4;
+		}
+		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+			y-=4;
+		}
+		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			x+=4;
+		}
+		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+			x-=4;
+		}
+		
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
 		batch.end();
 	}
 	
