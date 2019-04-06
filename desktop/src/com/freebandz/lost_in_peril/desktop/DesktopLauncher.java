d787dc7d1b3c32167d53037cb2226e1b0d8afe4a
Timothy Mardesen <tg-mardesen@wiu.edu>
2019-04-06 12:07:37 -0500

    First, working commit.

---
+++ desktop/src/com/freebandz/lost_in_peril/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.freebandz.lost_in_peril.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.freebandz.lost_in_peril.Lost_In_Peril;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		//add shit here before start screen shows
+		config.title = "Lost in Peril";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Lost_In_Peril(), config);
+	}
+}
