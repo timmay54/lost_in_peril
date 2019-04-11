d3ebc35978eb068cccb3cf5744a14a83f5c90ab3
Timothy Mardesen <tg-mardesen@wiu.edu>
2019-04-11 16:50:07 -0500

    can be exported as .JAR

--- core/src/com/freebandz/lost_in_peril/Lost_In_Peril.java
+++ core/src/com/freebandz/lost_in_peril/Lost_In_Peril.java
@@ -1,7 +1,9 @@
 package com.freebandz.lost_in_peril;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.freebandz.lost_in_peril.screens.MainMenuScreen;
 
 public class Lost_In_Peril extends Game {
@@ -10,14 +12,30 @@ public class Lost_In_Peril extends Game {
 	public static final int WIDTH = 1280;
 	public static final int HEIGHT = 720;
 	
+	private OrthographicCamera cam;
+	private StretchViewport viewport;
+	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		cam = new OrthographicCamera();
+		viewport = new StretchViewport(WIDTH, HEIGHT, cam);
+		viewport.apply();
+		cam.position.set(WIDTH/2, HEIGHT/2,0);
+		cam.update();
+		
 		this.setScreen(new MainMenuScreen(this)); //This determines first screen shown. was: new GameScreen(this)
 	}
 
 	@Override
 	public void render () {
+		batch.setProjectionMatrix(cam.combined);
 		super.render();
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		viewport.update(width, height);
+		super.resize(width, height);
 	}
 }
