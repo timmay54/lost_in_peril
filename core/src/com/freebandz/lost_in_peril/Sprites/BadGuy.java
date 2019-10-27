1f1ad9726ef572cb1c6732bb7594894301f9345e
tgm101 <tg-mardesen@wiu.edu>
2019-10-27 00:27:10 -0500

    annnnnddrrrooooiiiidddd

--- core/src/com/freebandz/lost_in_peril/Sprites/BadGuy.java
+++ core/src/com/freebandz/lost_in_peril/Sprites/BadGuy.java
@@ -1,11 +1,13 @@
 package com.freebandz.lost_in_peril.Sprites;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.World;
 import com.freebandz.lost_in_peril.screens.GameScreen;
 
 public class BadGuy extends Sprite{
 	World world;
-	public Badguy(GameScreen screen, float x, float y) {
+	public BadGuy(GameScreen screen, float x, float y) {
 		this.world = screen.getWorld();
+		//return;
 	}
 }
